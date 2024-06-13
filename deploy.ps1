2029272f4d5e090304464cc42575fc72b0763970
Amidu Dabor <36164952+Amidu-Dabor@users.noreply.github.com>
2024-06-13 03:45:00 +0300

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
-$StackName = 'awsvpcstack'
+$StackName = 'aws-vpc-stack'
 $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
 
 New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
